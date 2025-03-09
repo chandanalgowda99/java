387bda3790e5bae5aca2a2ee589e7a86a2cf0687
Chandana L <137513863+chandanalgowda99@users.noreply.github.com>
2025-03-09 10:52:53 +0530

    Add files via upload

---
+++ Vehicle.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class Vehicle{
+String brand;
+int speed;
+
+Vehicle(String brand,int speed){
+}
+
+void start(){
+System.out.println("Start");
+}
+
+void stop(){
+System.out.println("Stop");
+}
+
+void displayDetails(){
+System.out.println(brand);
+System.out.println(speed);
+}
+}
